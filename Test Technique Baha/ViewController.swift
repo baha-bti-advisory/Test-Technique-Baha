2af43c0d1d2e1a599faf98c5c7cf804efcf3ea0f
Baha Boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2022-12-06 19:03:21 +0100

    Initial Commit

---
+++ Test Technique Baha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Technique Baha
+//
+//  Created by Bahaeddine Boulaabi on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test Technique BahaUITests/Test_Technique_BahaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_Technique_BahaUITestsLaunchTests.swift
+//  Test Technique BahaUITests
+//
+//  Created by Bahaeddine Boulaabi on 06/12/2022.
+//
+
+import XCTest
+
+final class Test_Technique_BahaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
