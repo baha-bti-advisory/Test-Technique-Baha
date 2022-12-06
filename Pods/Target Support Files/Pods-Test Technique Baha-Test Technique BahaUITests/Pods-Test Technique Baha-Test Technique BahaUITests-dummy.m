ce4e519e4a11e5e5bcc857c09bb09da7070fc49b
Baha Boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2022-12-06 19:09:03 +0100

    Install Pod

---
+++ Pods/Target Support Files/Pods-Test Technique Baha-Test Technique BahaUITests/Pods-Test Technique Baha-Test Technique BahaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_Technique_Baha_Test_Technique_BahaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Test_Technique_Baha_Test_Technique_BahaUITests
+@end
---
+++ Pods/Target Support Files/Pods-Test Technique Baha/Pods-Test Technique Baha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_Technique_Baha : NSObject
+@end
+@implementation PodsDummy_Pods_Test_Technique_Baha
+@end
